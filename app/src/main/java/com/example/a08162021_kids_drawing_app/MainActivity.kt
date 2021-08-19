31612bc2f147ec239b2533f7a6a85b355be04427
Linh <d.linh1196@gmail.com>
2021-08-19 19:13:01 +0700

    Share image

--- app/src/main/java/com/example/a08162021_kids_drawing_app/MainActivity.kt
+++ app/src/main/java/com/example/a08162021_kids_drawing_app/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -200,6 +201,19 @@ class MainActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this@MainActivity, "Something went wrong while saving the file.", Toast.LENGTH_SHORT).show()
             }
+            MediaScannerConnection.scanFile(this@MainActivity, arrayOf(result),null) {
+                path, uri -> val shareIntent = Intent()
+                shareIntent.action = Intent.ACTION_SEND
+                shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+                shareIntent.type = "image/png"
+
+                startActivity(
+                    Intent.createChooser(
+                        shareIntent, "Share"
+                    )
+                )
+            }
+
         }
 
         private fun showProgressDialog() {
